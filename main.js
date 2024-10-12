c211d928714db843c3a428fb2aea343cea580c21
jarki26 <jarki26@gmail.com>
2024-10-12 19:41:11 +0200

    add duck

--- main.js
+++ main.js
@@ -1,259 +1,170 @@
 import * as THREE from "three"
-import {
-  color,
-  vec2,
-  pass,
-  linearDepth,
-  normalWorld,
-  gaussianBlur,
-  triplanarTexture,
-  texture,
-  objectPosition,
-  screenUV,
-  viewportLinearDepth,
-  viewportDepthTexture,
-  viewportSharedTexture,
-  mx_worley_noise_float,
-  positionWorld,
-  timerLocal,
-} from "three/tsl"
-
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
-
-import { OrbitControls } from "three/addons/controls/OrbitControls.js"
-
-import { GUI } from "three/addons/libs/lil-gui.module.min.js"
 
 import Stats from "three/addons/libs/stats.module.js"
 
+import { GUI } from "three/addons/libs/lil-gui.module.min.js"
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+import { Water } from "three/addons/objects/Water.js"
+import { Sky } from "three/addons/objects/Sky.js"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
+
+let container, stats
 let camera, scene, renderer
-let mixer, objects, clock
-let model, floor, floorPosition
-let postProcessing
-let controls
-let stats
+let controls, water, sun, mesh, duck
+let loader
 
 init()
 
 function init() {
-  camera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.25,
-    30
-  )
-  camera.position.set(3, 2, 4)
+  container = document.getElementById("container")
 
-  scene = new THREE.Scene()
-  scene.fog = new THREE.Fog(0x0487e2, 7, 25)
-  scene.backgroundNode = normalWorld.y.mix(color(0x0487e2), color(0x0066ff))
-  camera.lookAt(0, 1, 0)
+  //
 
-  const sunLight = new THREE.DirectionalLight(0xffe499, 5)
-  sunLight.castShadow = true
-  sunLight.shadow.camera.near = 0.1
-  sunLight.shadow.camera.far = 5
-  sunLight.shadow.camera.right = 2
-  sunLight.shadow.camera.left = -2
-  sunLight.shadow.camera.top = 1
-  sunLight.shadow.camera.bottom = -2
-  sunLight.shadow.mapSize.width = 2048
-  sunLight.shadow.mapSize.height = 2048
-  sunLight.shadow.bias = -0.001
-  sunLight.position.set(0.5, 3, 0.5)
-
-  const waterAmbientLight = new THREE.HemisphereLight(0x333366, 0x74ccf4, 5)
-  const skyAmbientLight = new THREE.HemisphereLight(0x74ccf4, 0, 1)
-
-  scene.add(sunLight)
-  scene.add(skyAmbientLight)
-  scene.add(waterAmbientLight)
-
-  clock = new THREE.Clock()
-
-  // animated model
-
-  const loader = new GLTFLoader()
-  loader.load("models/gltf/Michelle.glb", function (gltf) {
-    model = gltf.scene
-    model.children[0].children[0].castShadow = true
-
-    mixer = new THREE.AnimationMixer(model)
-
-    const action = mixer.clipAction(gltf.animations[0])
-    action.play()
-
-    scene.add(model)
-  })
-
-  // objects
-
-  const textureLoader = new THREE.TextureLoader()
-  const iceDiffuse = textureLoader.load("./textures/water.jpg")
-  iceDiffuse.wrapS = THREE.RepeatWrapping
-  iceDiffuse.wrapT = THREE.RepeatWrapping
-  iceDiffuse.colorSpace = THREE.NoColorSpace
-
-  const iceColorNode = triplanarTexture(texture(iceDiffuse))
-    .add(color(0x0066ff))
-    .mul(0.8)
-
-  const geometry = new THREE.IcosahedronGeometry(1, 3)
-  const material = new THREE.MeshStandardNodeMaterial({
-    colorNode: iceColorNode,
-  })
-
-  const count = 100
-  const scale = 3.5
-  const column = 10
-
-  objects = new THREE.Group()
-
-  for (let i = 0; i < count; i++) {
-    const x = i % column
-    const y = i / column
-
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.set(x * scale, 0, y * scale)
-    mesh.rotation.set(Math.random(), Math.random(), Math.random())
-    objects.add(mesh)
-  }
-
-  objects.position.set(
-    (column - 1) * scale * -0.5,
-    -1,
-    (count / column) * scale * -0.5
-  )
-
-  scene.add(objects)
-
-  // water
-
-  const timer = timerLocal(0.8)
-  const floorUV = positionWorld.xzy
-
-  const waterLayer0 = mx_worley_noise_float(floorUV.mul(4).add(timer))
-  const waterLayer1 = mx_worley_noise_float(floorUV.mul(2).add(timer))
-
-  const waterIntensity = waterLayer0.mul(waterLayer1)
-  const waterColor = waterIntensity
-    .mul(1.4)
-    .mix(color(0x0487e2), color(0x74ccf4))
-
-  // linearDepth() returns the linear depth of the mesh
-  const depth = linearDepth()
-  const depthWater = viewportLinearDepth.sub(depth)
-  const depthEffect = depthWater.remapClamp(-0.002, 0.04)
-
-  const refractionUV = screenUV.add(vec2(0, waterIntensity.mul(0.1)))
-
-  // linearDepth( viewportDepthTexture( uv ) ) return the linear depth of the scene
-  const depthTestForRefraction = linearDepth(
-    viewportDepthTexture(refractionUV)
-  ).sub(depth)
-
-  const depthRefraction = depthTestForRefraction.remapClamp(0, 0.1)
-
-  const finalUV = depthTestForRefraction
-    .lessThan(0)
-    .select(screenUV, refractionUV)
-
-  const viewportTexture = viewportSharedTexture(finalUV)
-
-  const waterMaterial = new THREE.MeshBasicNodeMaterial()
-  waterMaterial.colorNode = waterColor
-  waterMaterial.backdropNode = depthEffect.mix(
-    viewportSharedTexture(),
-    viewportTexture.mul(depthRefraction.mix(1, waterColor))
-  )
-  waterMaterial.backdropAlphaNode = depthRefraction.oneMinus()
-  waterMaterial.transparent = true
-
-  const water = new THREE.Mesh(
-    new THREE.BoxGeometry(50, 0.001, 50),
-    waterMaterial
-  )
-  water.position.set(0, 0, 0)
-  scene.add(water)
-
-  // floor
-
-  floor = new THREE.Mesh(
-    new THREE.CylinderGeometry(1.1, 1.1, 10),
-    new THREE.MeshStandardNodeMaterial({ colorNode: iceColorNode })
-  )
-  floor.position.set(0, -5, 0)
-  scene.add(floor)
-
-  // caustics
-
-  const waterPosY = positionWorld.y.sub(water.position.y)
-
-  let transition = waterPosY.add(0.1).saturate().oneMinus()
-  transition = waterPosY
-    .lessThan(0)
-    .select(transition, normalWorld.y.mix(transition, 0))
-    .toVar()
-
-  const colorNode = transition.mix(
-    material.colorNode,
-    material.colorNode.add(waterLayer0)
-  )
-
-  //material.colorNode = colorNode;
-  floor.material.colorNode = colorNode
-
-  // renderer
-
-  renderer = new THREE.WebGPURenderer(/*{ antialias: true }*/)
+  renderer = new THREE.WebGLRenderer()
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.setAnimationLoop(animate)
-  document.body.appendChild(renderer.domElement)
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
+  renderer.toneMappingExposure = 0.5
+  container.appendChild(renderer.domElement)
 
-  stats = new Stats()
-  document.body.appendChild(stats.dom)
+  //
+
+  scene = new THREE.Scene()
+
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  )
+  camera.position.set(30, 30, 100)
+
+  loader = new GLTFLoader()
+  loader.load(
+    "models/Rubber_Duck.glb",
+    function (gltf) {
+      const SCALE = 100
+      gltf.scene.scale.set(SCALE, SCALE, SCALE)
+      duck = gltf.scene
+      scene.add(duck)
+    },
+    undefined,
+    function (error) {
+      console.error(error)
+    }
+  )
+
+  //
+
+  sun = new THREE.Vector3()
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000)
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "textures/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  })
+
+  water.rotation.x = -Math.PI / 2
+
+  scene.add(water)
+
+  // Skybox
+
+  const sky = new Sky()
+  sky.scale.setScalar(10000)
+  scene.add(sky)
+
+  const skyUniforms = sky.material.uniforms
+
+  skyUniforms["turbidity"].value = 10
+  skyUniforms["rayleigh"].value = 2
+  skyUniforms["mieCoefficient"].value = 0.005
+  skyUniforms["mieDirectionalG"].value = 0.8
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180,
+  }
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer)
+  const sceneEnv = new THREE.Scene()
+
+  let renderTarget
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation)
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth)
+
+    sun.setFromSphericalCoords(1, phi, theta)
+
+    sky.material.uniforms["sunPosition"].value.copy(sun)
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize()
+
+    if (renderTarget !== undefined) renderTarget.dispose()
+
+    sceneEnv.add(sky)
+    renderTarget = pmremGenerator.fromScene(sceneEnv)
+    scene.add(sky)
+
+    scene.environment = renderTarget.texture
+  }
+
+  updateSun()
+
+  //
+
+  //   const geometry = new THREE.BoxGeometry(30, 30, 30)
+  //   const material = new THREE.MeshStandardMaterial({ roughness: 0 })
+
+  //   mesh = new THREE.Mesh(geometry, material)
+  //   scene.add(mesh)
+
+  //
 
   controls = new OrbitControls(camera, renderer.domElement)
-  controls.minDistance = 1
-  controls.maxDistance = 10
-  controls.maxPolarAngle = Math.PI * 0.9
-  controls.autoRotate = true
-  controls.autoRotateSpeed = 1
-  controls.target.set(0, 0.2, 0)
+  controls.maxPolarAngle = Math.PI * 0.495
+  controls.target.set(0, 10, 0)
+  controls.minDistance = 40.0
+  controls.maxDistance = 200.0
   controls.update()
 
-  // gui
+  //
+
+  stats = new Stats()
+  container.appendChild(stats.dom)
+
+  // GUI
 
   const gui = new GUI()
 
-  floorPosition = new THREE.Vector3(0, 0.2, 0)
+  const folderSky = gui.addFolder("Sky")
+  folderSky.add(parameters, "elevation", 0, 90, 0.1).onChange(updateSun)
+  folderSky.add(parameters, "azimuth", -180, 180, 0.1).onChange(updateSun)
+  folderSky.open()
 
-  gui.add(floorPosition, "y", -1, 1, 0.001).name("position")
+  const waterUniforms = water.material.uniforms
 
-  // post processing
-
-  const scenePass = pass(scene, camera)
-  const scenePassColor = scenePass.getTextureNode()
-  const scenePassDepth = scenePass.getLinearDepthNode().remapClamp(0.3, 0.5)
-
-  const waterMask = objectPosition(camera).y.greaterThan(
-    screenUV.y.sub(0.5).mul(camera.near)
-  )
-
-  const scenePassColorBlurred = gaussianBlur(scenePassColor)
-  scenePassColorBlurred.directionNode = waterMask.select(
-    scenePassDepth,
-    scenePass.getLinearDepthNode().mul(5)
-  )
-
-  const vignet = screenUV.distance(0.5).mul(1.35).clamp().oneMinus()
-
-  postProcessing = new THREE.PostProcessing(renderer)
-  postProcessing.outputNode = waterMask.select(
-    scenePassColorBlurred,
-    scenePassColorBlurred.mul(color(0x74ccf4)).mul(vignet)
-  )
+  const folderWater = gui.addFolder("Water")
+  folderWater
+    .add(waterUniforms.distortionScale, "value", 0, 8, 0.1)
+    .name("distortionScale")
+  folderWater.add(waterUniforms.size, "value", 0.1, 10, 0.1).name("size")
+  folderWater.open()
 
   //
 
@@ -268,24 +179,29 @@ function onWindowResize() {
 }
 
 function animate() {
+  render()
   stats.update()
+}
 
-  controls.update()
+function render() {
+  const time = performance.now() * 0.001
 
-  const delta = clock.getDelta()
+  //   mesh_animation(time)
+  duck && duck_animation(time)
 
-  floor.position.y = floorPosition.y - 5
+  water.material.uniforms["time"].value += 1.0 / 60.0
 
-  if (model) {
-    mixer.update(delta)
+  renderer.render(scene, camera)
+}
 
-    model.position.y = floorPosition.y
-  }
+function mesh_animation(time) {
+  mesh.position.y = Math.sin(time) * 20 + 5
+  mesh.rotation.x = time * 0.5
+  mesh.rotation.z = time * 0.51
+}
 
-  for (const object of objects.children) {
-    object.position.y = Math.sin(clock.elapsedTime + object.id) * 0.3
-    object.rotation.y += delta * 0.3
-  }
-
-  postProcessing.render()
+function duck_animation(time) {
+  duck.position.y = Math.sin(time) * 5 + 14
+  //   duck.rotation.x = time * 0.5
+  //   duck.rotation.z = time * 0.51
 }
